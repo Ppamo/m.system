c763ee424f904081504c96956369051e82ba28bf
Ppamo <pablo@ppamo.cl>
2016-08-15 14:13:18 -0300

    streamsRewrite - mock.trans.c draft

--- src/control.sh
+++ src/control.sh
@@ -4,6 +4,7 @@ SERVER=localhost
 WORKINGDIR=/tmp/mock-server
 PORT=8080
 MOCK_TRANS_PORT=10005
+LONG_MESSAGE="cmd:newad\nphone:98478466\nregion:15\nsource:android\ncategory:7060\naddress_number:2662\nremote_addr:179.4.239.14\nname:Juan Valdivia\nlang:es\nphone_hidden:0\nsubject:Cactus para regalar en matrimonios y eventos\ndigest_present:1\ndigest_present:1\ndigest_present:1\ndigest_present:1\ndigest_present:1\ndigest_present:1\nemail:juanenriquev@hotmail.com\ntype:s\nprice:1290\ngeoposition:-33.50285,-70.75915\nad_id:38716489\nstreet_id:93787\ndo_not_send_mail:1\nad_type:edit\naddress:Calle Monumento\nimage:9402572564.jpg\nimage:9438945668.jpg\nimage:9443505324.jpg\nimage:9413971704.jpg\nimage:9433894163.jpg\nimage:9473739081.jpg\ncompany_ad:1\npay_type:free\nblob:579:body\nbody:Cactus y suculentas surtidas aleatoriamente (30% cactus y 70% suculentas), en macetero de ceráca de 6 cm aprox. Decorados con piedras de colores, encintado en tono definido por el cliente, incluye tarjeta.#012No incluye despacho a domicilio#012Tiempo de entrega: Dependeráe las cantidades solicitadas y stock disponible. #012El valor del souvenir puede variar segúntidad de colores solicitados.#012Pedido míno:50 unid.#012Aviso vádo hasta el 31 de diciembre de 2016 o hasta agotar stock.#012Contacto y fono: Juan Enrique F:+56 9 84784665#012Consulte por nuestro sitio Online\n\npasswd:_1024_Nyc2J7jDyw8k4@?nf38e72a1cc95927105ad3efb79c2078a56a67a20\ncommunes:320\ngeoposition_is_precise:1\ncommit:1\nend\n"
 
 case "$1" in
 	start)
@@ -38,12 +39,16 @@ case "$1" in
 	clean)
 		echo "removing folder $WORKINGDIR"
 		rm -Rf $WORKINGDIR
+		mkdir -p $WORKINGDIR
 		echo
+		;;
+	list)
+		find $WORKINGDIR | less
 		;;
 	hit)
 		case "$2" in
 			1)
-				MSG="cmd:get_account_ads2\nads_quantity:500\naccount_id:1693703\npage_number:0\nall_ad_params:1\ncommit:1\nend\n"
+				MSG=$LONG_MESSAGE
 				;;
 			2)
 				MSG="newline:0A\ncommit:1\nad_id:36248711\ncmd:admail\nmail_type:half_time\nend\n"
@@ -54,7 +59,6 @@ case "$1" in
 			*)
 				MSG="cmd:test\nsubject:1\nend\n"
 		esac
-		printf "$MSG"
 		printf "$MSG" | nc localhost $MOCK_TRANS_PORT
 		;;
 	*)
---
+++ src/mock.trans.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define BUFFERSIZE 1024
+#define PORT 10005
+#define TRANSHOST "localhost"
+#define TRANSPORT 20005
+
+void error(char *msg) {
+	perror(msg);
+	exit(1);
+}
+
+int ensure_file_path(char *file_path){
+	char command[256];
+	sprintf(command, "mkdir -p $(dirname %s)", file_path);
+	system(command);
+}
+
+int isEOM(char *message){
+	if (strlen(message) == 4){
+		if (strstr(message, "end\n") != NULL){
+			return 1;
+		}
+	}
+	if (strstr(message, "\nend\0") != NULL){
+		return 1;
+	}
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	static const char *base_path="/tmp/mock-server/trans";
+	int optval = 1, n, reading = 1, connection_counter;
+	int fd_parent, fd_child, fd_trans, clientlen;
+	struct sockaddr_in server_addr, client_addr, trans_addr;
+	struct hostent *hostp, *trans;
+	char buffer_in[BUFFERSIZE], buffer_out[BUFFERSIZE], file_path[128];
+	char *hostaddrp, *stage="stage01";
+	FILE *fd_in, *fd_out;
+
+	fd_parent = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd_parent < 0) {
+		error("0> ERROR opening socket");
+	}
+
+	setsockopt(fd_parent, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+	bzero((char *) &server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons((unsigned short)PORT);
+
+	if (bind(fd_parent, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+		error("0> ERROR on binding");
+	}
+	if (listen(fd_parent, 5) < 0){
+		error("0> ERROR on listen");
+	}
+
+	// setup and test trans connection
+	fd_trans = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd_trans < 0)
+		error ("0> ERROR creating trans socket");
+	trans = gethostbyname(TRANSHOST);
+	if (trans == NULL)
+		error("0> ERROR trans host not found");
+	bzero((char *) &trans_addr, sizeof(trans_addr));
+	trans_addr.sin_family = AF_INET;
+	bcopy((char *)trans->h_addr, (char *)&trans_addr.sin_addr.s_addr, trans->h_length);
+	trans_addr.sin_port = htons(TRANSPORT);
+	n = connect(fd_trans, (struct sockaddr *)&trans_addr, sizeof(trans_addr));
+	if (n < 0)
+		error("0> ERROR connecting to trans");
+	// read from trans
+	bzero(buffer_out, BUFFERSIZE);
+	read(fd_trans, buffer_out, BUFFERSIZE);
+	shutdown(fd_trans, 2);
+	if (strcmp(buffer_out, "220 Welcome.\n") != 0)
+		error ("0> ERROR handshake with trans failed");
+	
+	clientlen = sizeof(client_addr);
+	connection_counter = 0;
+	while (1) {
+		connection_counter++;
+		fd_child = accept(fd_parent, (struct sockaddr *) &client_addr, &clientlen);
+		if (fd_child < 0) {
+			error("0> ERROR on accept");
+		}
+
+		// creating file
+		sprintf(file_path, "%s/%s/%i.in.dump", base_path, stage, connection_counter);
+		printf("%i> filepath = %s\n", connection_counter, file_path);
+		ensure_file_path(file_path);
+		fd_in = fopen(file_path, "w");
+		sprintf(file_path, "%s/%s/%i.out.dump", base_path, stage, connection_counter);
+		fd_out = fopen(file_path, "w");
+		if (fd_in == NULL)
+			error("ERROR could not create input log file");
+		if (fd_out == NULL)
+			error("ERROR could not create output log file");
+		
+		hostp = gethostbyaddr((const char *)&client_addr.sin_addr.s_addr, sizeof(client_addr.sin_addr.s_addr), AF_INET);
+		if (hostp == NULL){
+			perror("ERROR on gethostbyaddr");
+		}
+		hostaddrp = inet_ntoa(client_addr.sin_addr);
+		if (hostaddrp == NULL){
+			error("ERROR on inet_ntoa\n");
+		}
+		printf  ("%i> server established connection with %s (%s)\n", connection_counter, hostp->h_name, hostaddrp);
+		
+		// connect to trans
+		fd_trans = socket(AF_INET, SOCK_STREAM, 0);
+		n = connect(fd_trans, (struct sockaddr *) &trans_addr, sizeof(trans_addr));
+		if (n < 0){
+			printf("%i> ERROR connecting to trans - %i\n", connection_counter, n);
+		}
+
+		reading = 1;
+		while (reading){
+			bzero(buffer_in, BUFFERSIZE);
+			n = read(fd_child, buffer_in, BUFFERSIZE);
+			if (n == 0){
+				printf("%i> end of message\n", connection_counter);
+				reading = 0;
+				continue;
+			}
+			if (n < 0){
+				printf("%i> ERROR reading from socket", connection_counter);
+				reading = 0;
+				continue;
+			}
+			// printf("%i> server received %d bytes:\n", connection_counter, n);
+			write(fd_trans, buffer_in, n);
+			fwrite(buffer_in, 1, n, fd_in);
+			
+			if (isEOM(buffer_in)){
+				// printf("%i> EOM\n", connection_counter);
+				// read from trans socket
+				bzero(buffer_out, BUFFERSIZE);
+				n = read(fd_trans, buffer_out, BUFFERSIZE);
+				// printf("%i> readed %i chars:\n%s\n", connection_counter, n, buffer_out);
+				while (n > 0){
+					// printf("%i> writting back %i chars.\n", connection_counter, n);
+					write(fd_child, buffer_out, n);
+					fwrite(buffer_out, 1, n, fd_out);
+					bzero(buffer_out, BUFFERSIZE);
+					n = read(fd_trans, buffer_out, BUFFERSIZE);
+					if (n < 0)
+						perror("ERROR couldn't read from trans");
+				}
+				reading = 0;
+			}
+			else {
+				// printf("%i> Not EOM\n", connection_counter);
+			}
+		}
+		printf("%i> Closing connection\n", connection_counter);
+		fclose(fd_in);
+		fclose(fd_out);
+		close(fd_trans);
+		close(fd_child);
+		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+	}
+}
