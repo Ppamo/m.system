90b1e26345a66930d09ebfaf98b520815223fc0b
Pablo Mansilla Ojeda <pablo@schibsted.cl>
2016-09-20 02:00:29 -0300

    streamsRewrite - Add a debug function and a stop function to kill the running server

--- src/mock.trans.c
+++ src/mock.trans.c
@@ -5,6 +5,7 @@
 #include <netdb.h>
 #include <fcntl.h>
 #include <time.h>
+#include <signal.h>
 #include <sys/types.h> 
 #include <sys/ipc.h>
 #include <sys/shm.h>
@@ -21,6 +22,7 @@ struct shared_data {
 	int recording;
 	char stage[32];
 	time_t update_time;
+	pid_t pid;
 };
 
 char *str2md5(const char *str, int length) {
@@ -50,9 +52,29 @@ char *str2md5(const char *str, int length) {
     return out;
 }
 
-void error(char *msg) {
-	perror(msg);
+void error(char *msg, FILE *fd_log) {
+	if (fd_log == NULL) {
+		perror(msg);
+	} else {
+		fprintf(fd_log, "%s\n", msg);
+		fclose(fd_log);
+	}
 	exit(1);
+}
+
+void debug(FILE *fd, int connection_counter, char *msg){
+	time_t now;
+	time(&now);
+	char buffer[100];
+	strftime (buffer, 100, "%Y-%m-%d %H:%M:%S.000", localtime (&now));
+
+	if (connection_counter != -1){
+		fprintf(fd, "%s - [%i]: %s\n", buffer, connection_counter, msg);
+	}
+	else {
+		fprintf(fd, "%s - %s\n", buffer, msg);
+	}
+	fflush(fd);
 }
 
 int ensure_file_path(char *file_path){
@@ -79,16 +101,16 @@ struct shared_data *get_shmem(const char *base_path){
 	struct shared_data *data_ptr;
 
 	if ((key = ftok(base_path, 'R')) == -1) {
-		error("ERROR creating shared memmory key");
+		error("ERROR creating shared memmory key", NULL);
 	}
 
 	if ((shmid = shmget(key, sizeof(struct shared_data), 0644 | IPC_CREAT)) == -1) {
-		error("ERROR getting the shared memmory");
+		error("ERROR getting the shared memmory", NULL);
 	}
 
 	data_ptr = (struct shared_data *)shmat(shmid, (void *)0, 0);
 	if (data_ptr == (struct shared_data *)(-1)) {
-		error("ERROR attaching to shared memmory segment");
+		error("ERROR attaching to shared memmory segment", NULL);
 	}
 
 	return data_ptr;
@@ -97,6 +119,7 @@ struct shared_data *get_shmem(const char *base_path){
 void list_config(struct shared_data *data){
 	struct tm *timeinfo;
 	timeinfo = localtime(&(data->update_time));
+	printf("=> pid: %d\n", data->pid);
 	if (data->recording){
 		printf("=> mode: record\n");
 	} else {
@@ -119,13 +142,25 @@ void set_stage(struct shared_data *data, char value[100]){
 	list_config(data);
 } 
 
+void set_pid(struct shared_data *data, pid_t pid){
+	data->pid = pid;
+}
+
+void stop_server(struct shared_data *data){
+	if (data->pid > 0){
+		kill(data->pid, SIGQUIT);
+		data->pid = 0;
+		data->update_time = time(NULL);
+	}
+}
+
 void set_mode(struct shared_data *data, char value[20]){
 	if (strcmp(value, "play") == 0){
 		data->recording = 0;
 	} else if (strcmp(value, "record") == 0){
 		data->recording = 1;
 	} else {
-		error("ERROR unknown mode");
+		error("ERROR unknown mode", NULL);
 	}
 	data->update_time = time(NULL);
 	list_config(data);
@@ -140,8 +175,9 @@ int main(int argc, char **argv) {
 	struct stat stat_buf;
 	char buffer_in[BUFFERSIZE], buffer_out[BUFFERSIZE], file_path[128];
 	char *hostaddrp, *trans_host;
-	FILE *fd_in, *fd_out;
+	FILE *fd_in, *fd_out, *fd_log;
 	int mock_port, trans_port;
+	pid_t process_id = 0;
 
 	const char *base_path=getenv("HOME");
 	struct shared_data *shmem_data = get_shmem(base_path);
@@ -151,15 +187,20 @@ int main(int argc, char **argv) {
 	// set mode [play|record]
 	// get config
 	// server "port" "trans host" "trans port"
+	// stop
 	
 	if (argc > 1){
 		if (strcmp(argv[1], "server") == 0){
 			if (argc == 5){
-				mock_port = atoi(argv[2]);
-				trans_host = argv[3];
-				trans_port = atoi(argv[4]);
-				shmem_data->recording = 1;
-				printf("0> tunnel: %i:%s:%i\n", mock_port, trans_host, trans_port);
+				if (shmem_data->pid == 0){
+					mock_port = atoi(argv[2]);
+					trans_host = argv[3];
+					trans_port = atoi(argv[4]);
+					shmem_data->recording = 1;
+					printf("0> tunnel: %i:%s:%i\n", mock_port, trans_host, trans_port);
+				} else {
+					error("ERROR server seems to be already running", NULL);
+				}
 			}
 			else if (argc == 3){
 				mock_port = atoi(argv[2]);
@@ -167,7 +208,7 @@ int main(int argc, char **argv) {
 				printf("0> player: %i\n", mock_port);
 			}
 			else {
-				error("ERROR wrong number of parameter for server");
+				error("ERROR wrong number of parameter for server", NULL);
 			}
 		}
 		else if (strcmp(argv[1], "set") == 0){
@@ -177,10 +218,10 @@ int main(int argc, char **argv) {
 				} else if (strcmp(argv[2], "mode") == 0){
 					set_mode(shmem_data, argv[3]);
 				} else {
-					error("ERROR unknown variable to set");
+					error("ERROR unknown variable to set", NULL);
 				}
 			} else {
-				error("ERROR wrong number of parameter for set");
+				error("ERROR wrong number of parameter for set", NULL);
 			}
 
 			exit (0);
@@ -189,22 +230,55 @@ int main(int argc, char **argv) {
 			if (argc == 3 && strcmp(argv[2], "config") == 0){
 				list_config(shmem_data);
 			} else {
-				error("ERROR unknown param for get");
+				error("ERROR unknown param for get", NULL);
 			}
 			exit (0);
 		}
+		else if (strcmp(argv[1], "stop") == 0){
+			stop_server(shmem_data);
+			exit (0);
+		}
 		else {
-			error("0> ERROR command not found");
+			error("0> ERROR command not found", NULL);
 		}
 	}
 	else {
-		error("0> ERROR not enough parameters");
+		error("0> ERROR not enough parameters", NULL);
 	}
+
+	// XXX: fork and daemonization
+	process_id = fork();
+	if (process_id < 0){
+		error("0> ERROR fork failed!", NULL);
+	}
+	if (process_id > 0){
+		printf ("Killing parent process\n");
+		exit(0);
+	}
+	// get child pid
+	set_pid(shmem_data, getpid());
+
+	// create log file
+	fd_log = fopen("mock.log", "a");
+	if (fd_log == NULL){
+		error("ERROR fail to open log file", NULL);
+	}
+
+	// daemonization stuff
+	/*
+	umask(0);
+	setsid();
+	close(STDIN_FILENO);
+	close(STDOUT_FILENO);
+	close(STDERR_FILENO);
+	*/
+
 
 	fd_parent = socket(AF_INET, SOCK_STREAM, 0);
 	if (fd_parent < 0) {
-		error("0> ERROR opening socket");
+		error("0> ERROR opening socket", fd_log);
 	}
+	debug(fd_log, -1, "Starting!");
 
 	setsockopt(fd_parent, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
 	bzero((char *) &server_addr, sizeof(server_addr));
@@ -213,33 +287,33 @@ int main(int argc, char **argv) {
 	server_addr.sin_port = htons((unsigned short)mock_port);
 
 	if (bind(fd_parent, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-		error("0> ERROR on binding");
+		error("0> ERROR on binding", fd_log);
 	}
 	if (listen(fd_parent, 5) < 0){
-		error("0> ERROR on listen");
+		error("0> ERROR on listen", fd_log);
 	}
 
 	if (shmem_data->recording) {
 		// setup and test trans connection
 		fd_trans = socket(AF_INET, SOCK_STREAM, 0);
 		if (fd_trans < 0)
-			error ("0> ERROR creating trans socket");
+			error ("0> ERROR creating trans socket", fd_log);
 		trans = gethostbyname(trans_host);
 		if (trans == NULL)
-			error("0> ERROR trans host not found");
+			error("0> ERROR trans host not found", fd_log);
 		bzero((char *) &trans_addr, sizeof(trans_addr));
 		trans_addr.sin_family = AF_INET;
 		bcopy((char *)trans->h_addr, (char *)&trans_addr.sin_addr.s_addr, trans->h_length);
 		trans_addr.sin_port = htons(trans_port);
 		n = connect(fd_trans, (struct sockaddr *)&trans_addr, sizeof(trans_addr));
 		if (n < 0)
-			error("0> ERROR connecting to trans");
+			error("0> ERROR connecting to trans", fd_log);
 		// read from trans
 		bzero(buffer_out, BUFFERSIZE);
 		read(fd_trans, buffer_out, BUFFERSIZE);
 		shutdown(fd_trans, 2);
 		if (strcmp(buffer_out, "220 Welcome.\n") != 0)
-			error ("0> ERROR handshake with trans failed");
+			error ("0> ERROR handshake with trans failed", fd_log);
 	}
 	
 	clientlen = sizeof(client_addr);
@@ -248,39 +322,41 @@ int main(int argc, char **argv) {
 		connection_counter++;
 		fd_child = accept(fd_parent, (struct sockaddr *) &client_addr, &clientlen);
 		if (fd_child < 0) {
-			error("0> ERROR on accept");
+			error("0> ERROR on accept", fd_log);
 		}
 
 		if (shmem_data->recording) {
 			// creating file
 			sprintf(file_path, "%s/%s/%i.in.dump", base_path, shmem_data->stage, connection_counter);
-			printf("%i> filepath = %s\n", connection_counter, file_path);
+			debug(fd_log, connection_counter, "filepath: ");
+			debug(fd_log, -1, file_path);
 			ensure_file_path(file_path);
 			fd_in = fopen(file_path, "w");
 			sprintf(file_path, "%s/%s/%i.out.dump", base_path, shmem_data->stage, connection_counter);
 			fd_out = fopen(file_path, "w");
 			if (fd_in == NULL)
-				error("ERROR could not create input log file");
+				error("ERROR could not create input log file", fd_log);
 			if (fd_out == NULL)
-				error("ERROR could not create output log file");
+				error("ERROR could not create output log file", fd_log);
 		}
 		
 		hostp = gethostbyaddr((const char *)&client_addr.sin_addr.s_addr, sizeof(client_addr.sin_addr.s_addr), AF_INET);
 		if (hostp == NULL){
-			perror("ERROR on gethostbyaddr");
+			error("ERROR on gethostbyaddr", fd_log);
 		}
 		hostaddrp = inet_ntoa(client_addr.sin_addr);
 		if (hostaddrp == NULL){
-			error("ERROR on inet_ntoa\n");
+			error("ERROR on inet_ntoa\n", fd_log);
 		}
-		printf  ("%i> server established connection with %s (%s)\n", connection_counter, hostp->h_name, hostaddrp);
+		debug(fd_log, connection_counter, "server established connection with:");
+		debug(fd_log, -1, hostaddrp);
 		
 		if (shmem_data->recording) {
 			// connect to trans
 			fd_trans = socket(AF_INET, SOCK_STREAM, 0);
 			n = connect(fd_trans, (struct sockaddr *) &trans_addr, sizeof(trans_addr));
 			if (n < 0){
-				printf("%i> ERROR connecting to trans - %i\n", connection_counter, n);
+				debug(fd_log, connection_counter, "ERROR connecting to trans");
 			}
 		}
 
@@ -289,16 +365,16 @@ int main(int argc, char **argv) {
 			bzero(buffer_in, BUFFERSIZE);
 			n = read(fd_child, buffer_in, BUFFERSIZE);
 			if (n == 0){
-				printf("%i> end of message\n", connection_counter);
+				debug(fd_log, connection_counter, "end of message");
 				reading = 0;
 				continue;
 			}
 			if (n < 0){
-				printf("%i> ERROR reading from socket", connection_counter);
+				debug(fd_log, connection_counter, "ERROR reading from socket");
 				reading = 0;
 				continue;
 			}
-			printf("%i> server received %d bytes:\n", connection_counter, n);
+			debug(fd_log, connection_counter, "server received bytes:");
 
 			if (shmem_data->recording){
 				write(fd_trans, buffer_in, n);
@@ -306,28 +382,32 @@ int main(int argc, char **argv) {
 			}
 			
 			if (is_eom(buffer_in)){
-				printf("%i> EOM\n", connection_counter);
+				debug(fd_log, connection_counter, "EOM");
 				if (shmem_data->recording){
 					// read from trans socket
 					bzero(buffer_out, BUFFERSIZE);
 					n = read(fd_trans, buffer_out, BUFFERSIZE);
-					printf("%i> readed %i chars:\n%s\n", connection_counter, n, buffer_out);
+					debug(fd_log, connection_counter, "readed:");
+					debug(fd_log, -1, buffer_out);
 					while (n > 0){
-						printf("%i> writting back %i chars.\n", connection_counter, n);
+						debug(fd_log, connection_counter, "writting back:");
+						debug(fd_log, -1, buffer_out);
 						write(fd_child, buffer_out, n);
 						fwrite(buffer_out, 1, n, fd_out);
 						bzero(buffer_out, BUFFERSIZE);
 						n = read(fd_trans, buffer_out, BUFFERSIZE);
-						if (n < 0)
-							perror("ERROR couldn't read from trans");
+						if (n < 0){
+							error("ERROR couldn't read from trans", fd_log);
+						}
 					}
 				} else {
-					printf ("%i> EOM: retrieving file\n", connection_counter);
+					debug(fd_log, connection_counter, "EOM: retrieving file");
 					sprintf(file_path, "%s/%s/%i.out.dump", base_path, shmem_data->stage, connection_counter);
-					printf("%i> filepath = %s\n", connection_counter, file_path);
+					debug(fd_log, connection_counter, "filepath:");
+					debug(fd_log, -1, file_path);
 					fd_dump = open(file_path, O_RDONLY);
 					if (fd_dump == -1){
-						printf("%i> ERROR failed to open file\n", connection_counter);
+						debug(fd_log, connection_counter, "ERROR failed to open file");
 						continue;
 					}
 
@@ -338,13 +418,13 @@ int main(int argc, char **argv) {
 				reading = 0;
 			}
 		}
-		printf("%i> Closing connection\n", connection_counter);
+		debug(fd_log, connection_counter, "Closing connection");
 		if (shmem_data->recording){
 			fclose(fd_in);
 			fclose(fd_out);
 			close(fd_trans);
 		}
 		close(fd_child);
-		printf("- - - - - - - - - - - - - - - - - - - - - - - - - - -\n");
+		debug(fd_log, -1, "- - - - - - - - - - - - - - - - - - -");
 	}
 }
